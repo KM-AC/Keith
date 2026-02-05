ce79ba25b0cb02007fa7cdd0ccc065ab49c81d37
Keith <keith.milne@btinternet.com>
2026-02-05 10:13:57 +0000

    update
    update2

--- index.js
+++ index.js
@@ -11,10 +11,40 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js on Vercel!");
 });
 
-// Example API route
-app.get("/api/data", (req, res) => {
-  res.json({ message: "This is API data from Vercel" });
+const sqlite3 = require('sqlite3').verbose();
+
+// the name of the database file e.g. ./mydb.sqlite
+const db = new sqlite3.Database('./dbtests/mydb.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
 });
+// select all
+db.each('SELECT * FROM posts', (err, row) => {
+  if (err) {
+    throw err;
+  }
+  
+  console.log(row);
+}, () => {
+  // called when all rows have been retrieved
+});
+
+// INSERT A RECORD
+function createPost(db, title, content) {
+  const stmt = db.prepare(`
+    INSERT INTO posts (title, content) VALUES (?, ?)
+  `);
+  
+  stmt.run(title, content);
+  stmt.finalize();
+}
+
+// Usage:
+createPost(db, 'testagain', 'brilliant'); 
+
+db.close();
 
 // Start server (only for local dev)
 if (require.main === module) {
